51a6c5b3cae8f795d4d678da766e54d8499537a1
DeveloperAmrit <kittyamrit005@gmail.com>
2026-01-10 00:17:04 +0530

    modified:   src/components/home/FeaturesSection.tsx

--- src/components/home/FeaturesSection.tsx
+++ src/components/home/FeaturesSection.tsx
@@ -49,7 +49,7 @@ const FeaturesSection = () => {
       opacity: 1,
       y: 0,
       transition: {
-        type: "spring",
+        type: "spring" as const,
         stiffness: 100,
         damping: 10
       }
